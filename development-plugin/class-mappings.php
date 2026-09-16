fe7897c4183191fe2ec9154ff08508b8254107fe
Brian Henry <BrianHenryIE@gmail.com>
2026-09-16 12:42:00 -0700

    lint

--- development-plugin/class-mappings.php
+++ development-plugin/class-mappings.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * Fix `plugin_basename()` and `plugins_url()` issues when mapping in `wp-env`.
+ *
+ * @package brianhenryie/bh-wp-order-email-reconcile
+ */
 
 namespace BrianHenryIE\WP_Order_Email_Reconcile_Test_Plugin;
 
+/**
+ * Directly edits a global; does some string replacements that should be safe when run in an irrelevant environment.
+ */
 class Mappings {
 
+	/**
+	 * Update `$GLOBALS['wp_plugin_paths']` on `init`, and `plugins_url` on every run.
+	 */
 	public function register_hooks(): void {
 		add_action( 'init', array( $this, 'wp_plugin_paths' ) );
 		add_filter( 'plugins_url', array( $this, 'plugins_url_fix' ), 10, 3 );
@@ -13,6 +24,7 @@ class Mappings {
 	 * Fix for mapped directories. I.e. vendor is not under `wp-content/plugins/development-plugins`.
 	 *
 	 * @see plugin_basename()
+	 * @hooked init
 	 */
 	public function wp_plugin_paths(): void {
 		global $wp_plugin_paths;
@@ -30,9 +42,14 @@ class Mappings {
 	 * http://localhost:8888/wp-content/plugins/development-plugin/vendor/brianhenryie/bh-wp-private-uploads/includes/admin/assets/bh-wp-private-uploads-admin.js
 	 * http://localhost:8888/wp-content/uploads/bh-wp-mailboxes/vendor/brianhenryie/bh-wp-private-uploads/includes/admin/assets/bh-wp-private-uploads-admin.js
 	 *
+	 * @see plugins_url()
 	 * @hooked plugins_url
+	 *
+	 * @param string $url The URL we might need to modify.
+	 * @param string $_path Path relative to the URL to the plugins directory.
+	 * @param string $_plugin The plugin file path to be relative to.
 	 */
-	public function plugins_url_fix( $url, $_path, $_plugin ) {
+	public function plugins_url_fix( string $url, string $_path, string $_plugin ): string {
 		$url = str_replace( 'wp-content/plugins/var/www/html/', '', $url );
 		$url = str_replace( 'plugins/development-plugin/vendor', 'uploads/bh-wp-order-email-reconcile/vendor', $url );
 		$url = str_replace( 'plugins/development-plugin/includes', 'uploads/bh-wp-order-email-reconcile/includes', $url );
